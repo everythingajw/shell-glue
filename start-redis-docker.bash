3059c9628fcd992c830ddd4722ff926e767148a5
Anthony Webster <ajwebster1@gmail.com>
2024-10-17 20:03:55 -0400

    Fix redis docker script

--- start-redis-docker.bash
+++ start-redis-docker.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-exec sudo docker run -d --name redis-stack -p 6379:6379 redis/redis-stack-server:latest
+exec sudo docker run --rm -d --name redis-stack -p 6379:6379 redis/redis-stack-server:latest
 
